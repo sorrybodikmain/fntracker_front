f80f1cd9333d304e35d90c76d5fe782c676e5fd0
Bogdan Mazur <zochit21@gmail.com>
2023-04-08 23:45:21 +0300

    fixed api

--- src/libs/apiFetcher.ts
+++ src/libs/apiFetcher.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const fetcher = async (url: string) =>
 	await axios
-		.get(url, { headers: { authorization: process.env.REACT_APP_API_TOKEN } })
+		.get(url, { headers: { authorization: '0e10a7c8-de62b507-fb57e503-d359a4a2' } })
 		.then(res => res.data)
 		.catch(res => console.log(res))
 
